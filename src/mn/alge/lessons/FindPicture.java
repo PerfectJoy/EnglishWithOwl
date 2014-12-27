6d1c40b5476c79a0932ff970f216de317a27e7b4
GoldenLight <mr.tuulai@gmail.com>
2014-12-27 19:32:09 +0800

    FindPicture part 1.5

---
+++ src/mn/alge/lessons/FindPicture.java
@@ -0,0 +1,90 @@
+package mn.alge.lessons;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+
+import mn.alge.db.DBAdapter;
+import mn.alge.db.ImageAdapter;
+import mn.alge.db.Word;
+import mn.alge.english.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class FindPicture extends Activity {
+	ArrayList<Word> wordList = new ArrayList<Word>();
+	ArrayList<Word> wordList4 = new ArrayList<Word>(4);
+	private TextToSpeech tts;
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.find_picture);
+		tts = new TextToSpeech(getApplicationContext(),
+				new TextToSpeech.OnInitListener() {
+					@Override
+					public void onInit(int status) {
+						if (status != TextToSpeech.ERROR) {
+							tts.setLanguage(Locale.ENGLISH);
+						}
+					}
+				});
+		DBAdapter mDbHelper = new DBAdapter(this);
+		mDbHelper.createDatabase();
+		mDbHelper.open();
+
+		
+		wordList = mDbHelper.retrieveWordsFindPic();
+		Collections.shuffle(wordList);
+		wordList4.add(wordList.get(0));
+		wordList4.add(wordList.get(1));
+		wordList4.add(wordList.get(2));
+		wordList4.add(wordList.get(3));
+
+		mDbHelper.close();
+
+		GridView gridView = (GridView) findViewById(R.id.grid_view_find_picture);
+		// Instance of ImageAdapter Class
+		gridView.setAdapter(new ImageAdapter(this, wordList4));
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				tts.speak(wordList4.get(position).getEnglish(),
+						TextToSpeech.QUEUE_FLUSH, null);
+			}
+		});
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		this.finish();
+		super.onBackPressed();
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+}
